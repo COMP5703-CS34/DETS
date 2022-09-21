cf64443f4e09022863beb03b98308e5fa3e88c10
Yiwei Xu <tzxuyiwei100@126.com>
2022-09-21 11:47:01 -0700

    Backend: Connect to Hyperledger without problem

--- backend/src/main/java/com/backend/chain/hyperledger/HyperledgerConnection.java
+++ backend/src/main/java/com/backend/chain/hyperledger/HyperledgerConnection.java
@@ -2,21 +2,21 @@ package com.backend.chain.hyperledger;
 
 import org.hyperledger.fabric.gateway.*;
 
-import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 
 public class HyperledgerConnection {
 
-    private static final Path NETWORK_CONFIG_PATH = Paths.get("src", "main", "resources", "connection.yaml");
-    private static final Path WALLET_PATH = Paths.get("src", "main", "resources", "wallets");
+    private static final Path NETWORK_CONFIG_PATH = Paths.get("backend", "src", "main", "resources", "connection.yaml");
+    private static final Path WALLET_PATH = Paths.get("backend", "src", "main", "resources", "wallets");
     private static Gateway gateway = null;
     private static Contract contract = null;
 
     public static Contract getInstance() {
         if (gateway == null) {
             try {
+                HyperledgerEnrollAdmin.enrollAdmin();
                 Wallet wallet = Wallets.newFileSystemWallet(WALLET_PATH);
 
                 Gateway.Builder builder = Gateway.createBuilder();
@@ -27,10 +27,12 @@ public class HyperledgerConnection {
                 contract = network.getContract("elec-chaincode");
 
                 byte[] bytes = contract.evaluateTransaction("query", "Plant");
+                System.out.println("After doing query");
                 System.out.println(new String(bytes));
-            } catch (IOException | ContractException e) {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
+
         }
         return contract;
     }
