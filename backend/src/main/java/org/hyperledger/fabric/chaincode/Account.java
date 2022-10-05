a2d25c24ebc69307cc793cf8ee47f993a37dbc32
Evan Xu <tzxuyiwei100@126.com>
2022-10-05 00:07:53 -0700

    Backend: fix account

--- backend/src/main/java/org/hyperledger/fabric/chaincode/Account.java
+++ backend/src/main/java/org/hyperledger/fabric/chaincode/Account.java
@@ -18,62 +18,7 @@ public class Account implements Serializable {
     }
 
     public String getAccountId() {
-        return accountId;package org.hyperledger.fabric.chaincode;
-
-        import java.io.Serializable;
-        
-        
-        public class Account implements Serializable {
-            private String accountId;
-            private double elecAmount;
-            private double balance;
-            private String password;
-            private String identity;
-        
-            public Account(String accountId, double elecAmount, double balance, String password, String identity) {
-                this.accountId = accountId;
-                this.elecAmount = elecAmount;
-                this.balance = balance;
-                this.password = password;
-                this.identity = identity;
-            }
-        
-            public String getAccountId() {
-                return accountId;
-            }
-        
-            public double getElecAmount() {
-                return elecAmount;
-            }
-        
-            public double getBalance() {
-                return balance;
-            }
-        
-            public String getPassword() {
-                return password;
-            }
-        
-            public String getIdentity() {
-                return identity;
-            }
-        
-            public void setElecAmount(double elecAmount) {
-                this.elecAmount = elecAmount;
-            }
-        
-            public void setBalance(double balance) {
-                this.balance = balance;
-            }
-        
-            public void setPassword(String password) {
-                this.password = password;
-            }
-        
-            public void setIdentity(String identity) {
-                this.identity = identity;
-            }
-        }
+        return accountId;
     }
 
     public double getElecAmount() {
