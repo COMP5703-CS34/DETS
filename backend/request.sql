fe4dc409ee4d615a6b3ff380118245be540633f1
Evan Xu <tzxuyiwei100@126.com>
2022-10-11 06:43:17 -0700

    DB: add new attribute

--- backend/request.sql
+++ backend/request.sql
@@ -1,8 +1,9 @@
-drop table request;
+drop table `request`;
 CREATE TABLE `request` (
                 id tinyint primary key not null auto_increment,
-               `from` varchar(500) NOT NULL,
+                `from` varchar(500) NOT NULL,
                 `to` varchar(500) NOT NULL,
+                `bargainingUser` varchar(500) NOT NULL,
                 type boolean,
                 amount double,
                 price double,
