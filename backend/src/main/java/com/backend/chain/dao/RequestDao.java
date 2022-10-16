bff1400611539a3c6b6f2b046632d7c7f565d36a
Cheneverstop <91403778+Cheneverstop@users.noreply.github.com>
2022-10-16 18:07:04 +0800

    add comment

--- backend/src/main/java/com/backend/chain/dao/RequestDao.java
+++ backend/src/main/java/com/backend/chain/dao/RequestDao.java
@@ -7,16 +7,39 @@ import java.util.List;
 
 @Mapper
 public interface RequestDao {
+    /**
+     * add new request record to db
+     * @param req
+     * @return
+     */
     @Insert("insert into request(`from`,`to`,type,amount,price,status,createTime,updateTime,bargainingUser)" +
             "values (#{from},#{to},#{type},#{amount},#{price},#{status}, #{createTime}, #{updateTime}, #{bargainingUser})")
     int addRequest(Request req);
 
+    /**
+     * select all request record of user
+     * @param user
+     * @return
+     */
     @Select("select id,`from`,`to`,type,amount,price,status,createTime,updateTime,bargainingUser from request where `from`=#{user} or `to` = #{user}")
     List<Request> getAllByUser(String user);
 
+    /**
+     * update request status, bargainingUser, price by id
+     * @param id
+     * @param price
+     * @param bargainingUser
+     * @param status
+     * @return
+     */
     @Update("update request set status = #{status} , price = #{price} , bargainingUser = #{bargainingUser} where id = #{id}")
     int updateRequest(int id, double price,String bargainingUser, int status);
 
+    /**
+     * delete request by id
+     * @param id
+     * @return
+     */
     @Delete("delete from request where id = #{id}")
     int deleteRequest(int id);
 }
