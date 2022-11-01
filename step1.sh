f858922a15261f285bd56adf71f40abc219671eb
manya201 <52134518+manya201@users.noreply.github.com>
2022-11-02 00:29:58 +0800

    All: enable auto clearup for mysql

--- step1.sh
+++ step1.sh
@@ -5,4 +5,8 @@ sudo ./minifab cleanup
 wait
 git restore *
 wait
+cd ..
+sh initMysql.sh
+wait
+cd chaincode
 sudo ./minifab up -o stateA.elec.com -i 2.1 -n elec-chaincode -l java -v 1.0 -d true -e true -p '"init"'
