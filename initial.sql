61459da85066f7348f4b74d12a20847f1f19541a
manya201 <52134518+manya201@users.noreply.github.com>
2022-11-02 00:27:59 +0800

    All: add sql for table creating

---
+++ initial.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `request` (
+                id tinyint primary key not null auto_increment,
+                `from` varchar(500) NOT NULL,
+                `to` varchar(500) NOT NULL,
+                `bargainingUser` varchar(500) NOT NULL,
+                type boolean,
+                amount double,
+                price double,
+                createTime int,
+                updateTime int,
+                status int
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
