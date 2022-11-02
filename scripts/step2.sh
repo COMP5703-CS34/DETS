855e940acc5b419e22dc48523c8585b24c04b2b5
manya201 <52134518+manya201@users.noreply.github.com>
2022-11-02 20:29:35 +0800

    All: adjust directory

--- scripts/step2.sh
+++ scripts/step2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo rm backend/src/main/resources/connection.yaml
-rm -rf backend/src/main/resources/wallets
-cp chaincode/vars/profiles/mychannel_connection_for_gosdk.yaml backend/src/main/resources/connection.yaml
+sudo rm ../backend/src/main/resources/connection.yaml
+rm -rf ../backend/src/main/resources/wallets
+cp ../chaincode/vars/profiles/mychannel_connection_for_gosdk.yaml ../backend/src/main/resources/connection.yaml
 wait
 gnome-terminal -t "Frontend: Vue" --window -x bash -c "source vuescript.sh; exec bash"
 sleep 1s
