27559b0eb4acd4b0466027e899289fe0f09d6c75
manya201 <52134518+manya201@users.noreply.github.com>
2022-11-02 20:28:57 +0800

    All: adjust directory

--- scripts/step1.sh
+++ scripts/step1.sh
@@ -1,12 +1,12 @@
 #! #!/bin/bash
 
-cd chaincode
+cd ../chaincode
 sudo ./minifab cleanup
 wait
 git restore *
 wait
-cd ..
+cd ../scripts
 sh initMysql.sh
 wait
-cd chaincode
+cd ../chaincode
 sudo ./minifab up -o stateA.elec.com -i 2.1 -n elec-chaincode -l java -v 1.0 -d true -e true -p '"init"'
